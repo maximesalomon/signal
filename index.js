e903230ce47d3218853752bb6f6c1b841dc17c5f
Maxime Salomon <maxime@croissant.io>
2020-02-04 10:59:45 +0100

    add POST signals endpoint

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ server.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+server.post('/api/signals', (req, res) => {
+    res.send('Buying signal successfully processed!')
+})
+
 server.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
