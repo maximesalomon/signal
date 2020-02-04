4872d1be27fd4d0423a7fec9075956916ced4b4a
Maxime Salomon <maxime@croissant.io>
2020-02-04 17:38:33 +0100

    POST signal

--- index.js
+++ index.js
@@ -21,24 +21,32 @@ server.get("/api/signals", (req, res) => {
 });
 
 // GET signal by id
-server.get('api/signals/:id', (req, res) => {
-    const { id } = req.params;
-  
-    db('signals').where({ id })
+server.get("api/signals/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("signals")
+    .where({ id })
     .then(signal => {
       if (signal.length) {
         res.json(signal);
       } else {
-        res.status(404).json({ message: 'Could not find signal with given id.' })
-     }})
-    .catch (err => {
-      res.status(500).json({ message: 'Failed to get signal.' });
+        res
+          .status(404)
+          .json({ message: "Could not find signal with given id." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get signal." });
     });
-  });
+});
 
-
+// POST signal
 server.post("/api/signals", (req, res) => {
-  res.send("Buying signal successfully processed!");
+    db('signals')
+    .insert({ /* TODO */ })
+    .then(signal => {
+        res.send(`Signal ${signal.id} has been successfully processed!`);
+    })
 });
 
 server.listen(PORT, () =>
