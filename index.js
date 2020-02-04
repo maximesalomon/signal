d439441aa424038fa9ce5d97c4f470c563311da5
Maxime Salomon <maxime@croissant.io>
2020-02-04 17:31:08 +0100

    add knex + sqlite3

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require('express');
+const express = require("express");
+const db = require("../data/db.js");
 
 const server = express();
-
 const PORT = 7000;
 
-server.get('/', (req, res) => {
-  res.send('Hello, world!');
+server.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
-server.post('/api/signals', (req, res) => {
-    res.send('Buying signal successfully processed!')
-})
+server.get("/api/signals", (req, res) => {
+  db("signals")
+    .then(signals => {
+      res.json(signals);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get signals" });
+    });
+});
+
+server.post("/api/signals", (req, res) => {
+  res.send("Buying signal successfully processed!");
+});
 
 server.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
