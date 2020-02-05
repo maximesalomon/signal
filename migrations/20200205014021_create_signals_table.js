22c77be6b26cd34ed379cc7bddfe4f9f37dc41d9
Maxime Salomon <maxime@croissant.io>
2020-02-05 01:40:51 +0100

    text to string

--- migrations/20200205014021_create_signals_table.js
+++ migrations/20200205014021_create_signals_table.js
@@ -2,16 +2,16 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("signals", tbl => {
     tbl.increments();
     tbl
-      .text("uuid", 128)
+      .string("uuid", 128)
       .unique()
       .notNullable();
-    tbl.text("company", 128).notNullable();
-    tbl.text("title", 128).notNullable();
-    tbl.text("url", 128).notNullable();
-    tbl.text("location", 128);
-    tbl.text("first_seen_at", 128);
-    tbl.text("last_seen_at", 128);
-    tbl.text("last_processed_at", 128);
+    tbl.string("company", 128).notNullable();
+    tbl.string("title", 128).notNullable();
+    tbl.string("url", 128).notNullable();
+    tbl.string("location", 128);
+    tbl.string("first_seen_at", 128);
+    tbl.string("last_seen_at", 128);
+    tbl.string("last_processed_at", 128);
     tbl.boolean("job_opening_close").notNullable();
   });
 };
