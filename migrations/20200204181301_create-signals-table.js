78d58f3a4fdd13731dd80761da71093f9cbec7ea
Maxime Salomon <maxime@croissant.io>
2020-02-05 01:15:17 +0100

    create signals table

--- migrations/20200204181301_create-signals-table.js
+++ migrations/20200204181301_create-signals-table.js
@@ -1,8 +1,21 @@
-
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("signals", tbl => {
+    tbl.increments();
+    tbl
+      .text("uuid", 128)
+      .unique()
+      .notNullable();
+    tbl.text("title", 128).notNullable();
+    tbl.text("url", 128).notNullable();
+    tbl.text("location", 128);
+    tbl.text("first_seen_at", 128);
+    tbl.text("last_seen_at", 128);
+    tbl.text("last_processed_at", 128);
+    tbl.boolean("job_opening_close").notNullable();
+    tbl.decimal("budget").notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("signals");
 };
