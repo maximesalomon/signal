d3164bdeaf354e05aaed66a62d34e8bed0ff8219
Maxime Salomon <maxime@croissant.io>
2020-02-06 01:43:37 +0100

    fix knex db_config

--- data/db.js
+++ data/db.js
@@ -1,14 +1,4 @@
 const knex = require('knex');
 const config = require('../knexfile');
-
-switch (process.env.NODE_ENV){
-  case 'production':
-    const db_config = config.production;
-    break;
-  default:
-    const db_config = config.development
-}
-
-
-const db = knex(db_config);
+const db = knex(config.production);
 module.exports = db;
