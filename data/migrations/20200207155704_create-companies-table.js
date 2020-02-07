7451f99d998f499bff2c6b0116aadf2ccb244627
Maxime Salomon <maxime@croissant.io>
2020-02-07 16:20:30 +0100

    update companies rows

--- data/migrations/20200207155704_create-companies-table.js
+++ data/migrations/20200207155704_create-companies-table.js
@@ -1,16 +1,12 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable("companies", tbl => {
-      tbl.increments();
-      tbl.string("name", 128).notNullable();
-      tbl.string("url", 128)
-      tbl.string("location", 128)
-      tbl.decimal("jobs", 128)
-      tbl.decimal("open_jobs", 128)
-      tbl.decimal("closed_jobs", 128)
-    });
-  };
-  
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists("companies");
-  };
-  
+  return knex.schema.createTable("companies", tbl => {
+    tbl.increments();
+    tbl.string("name", 128).notNullable();
+    tbl.string("url", 128).notNullable();
+    tbl.string("location", 128).notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("companies");
+};
